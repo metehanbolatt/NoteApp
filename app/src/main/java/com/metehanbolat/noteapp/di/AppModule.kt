b372d866eb49e8efdb40f20062d1defa16de9434
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-07 17:39:10 +0300

    AddEditNoteViewModel

--- app/src/main/java/com/metehanbolat/noteapp/di/AppModule.kt
+++ app/src/main/java/com/metehanbolat/noteapp/di/AppModule.kt
@@ -5,10 +5,7 @@ import androidx.room.Room
 import com.metehanbolat.noteapp.feature_note.data.data_source.NoteDataBase
 import com.metehanbolat.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.metehanbolat.noteapp.feature_note.domain.repository.NoteRepository
-import com.metehanbolat.noteapp.feature_note.domain.use_case.AddNote
-import com.metehanbolat.noteapp.feature_note.domain.use_case.DeleteNote
-import com.metehanbolat.noteapp.feature_note.domain.use_case.GetNotes
-import com.metehanbolat.noteapp.feature_note.domain.use_case.NoteUseCases
+import com.metehanbolat.noteapp.feature_note.domain.use_case.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -41,7 +38,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
--- app/src/main/java/com/metehanbolat/noteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/metehanbolat/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -3,5 +3,6 @@ package com.metehanbolat.noteapp.feature_note.domain.use_case
 data class NoteUseCases(
     val getNotes: GetNotes,
     val deleteNote: DeleteNote,
-    val addNote: AddNote
+    val addNote: AddNote,
+    val getNote: GetNote
 )
